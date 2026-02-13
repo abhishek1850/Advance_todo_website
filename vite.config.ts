71c227e6c5a3bf08de03f866af58a4e37dbd8e05
abhishek1850 <purohitabhishek1850@gmail.com>
2026-02-13 10:42:28 +0530

    Finalizing production readiness: CSP updates and Llama 3.3 config

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
         "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://*.googleapis.com https://*.gstatic.com",
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
         "font-src 'self' https://fonts.gstatic.com",
-        "img-src 'self' data: blob: https: http:",
-        "connect-src 'self' https://*.googleapis.com https://*.google.com https://*.firebaseio.com https://*.cloudfunctions.net https://*.supabase.co wss://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://generativelanguage.googleapis.com",
+        "img-src 'self' data: blob: https: http: https://lh3.googleusercontent.com",
+        "connect-src 'self' https://*.googleapis.com https://*.google.com https://*.firebaseio.com https://*.cloudfunctions.net https://*.supabase.co wss://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://generativelanguage.googleapis.com https://api.groq.com https://fonts.gstatic.com",
         "frame-src 'self' https://*.firebaseapp.com https://*.google.com https://apis.google.com",
       ].join('; '),
       // Prevent clickjacking
